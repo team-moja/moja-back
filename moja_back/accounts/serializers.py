8043dd620743ce6064a9b56397d23f7a36e7a1c9
yunjaeeun <yunjaeeun12@naver.com>
2024-11-19 13:42:07 +0900

    Resolves: 회원 조회 기능 구현
    1. 회원 단일 조회 구현
    2. 회원 다중 조회(아이디, 닉네임, 포인트, 등급)

--- moja_back/accounts/serializers.py
+++ moja_back/accounts/serializers.py
@@ -3,18 +3,28 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 from .models import User
 
 class CustomRegisterSerializer(RegisterSerializer):
-  nickname = serializers.CharField(required=True, max_length=255)
-  birth_date = serializers.DateField()
-  user_monthly_income = serializers.IntegerField()
-  user_monthly_expenses = serializers.IntegerField()
+    nickname = serializers.CharField(required=True, max_length=255)
+    birth_date = serializers.DateField()
+    user_monthly_income = serializers.IntegerField()
+    user_monthly_expenses = serializers.IntegerField()
 
-  def get_cleaned_data(self):
-    return {
-      'username': self.validated_data.get('username', ''),
-      'password1': self.validated_data.get('password1', ''),
-      'nickname': self.validated_data.get('nickname', ''),
-      'email': self.validated_data.get('email', ''),
-      'birth_date': self.validated_data.get('birth_date', ''),
-      'user_monthly_income': self.validated_data.get('user_monthly_income', ''),
-      'user_monthly_expenses': self.validated_data.get('user_monthly_expenses', ''),
-    }
+    def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'nickname': self.validated_data.get('nickname', ''),
+            'email': self.validated_data.get('email', ''),
+            'birth_date': self.validated_data.get('birth_date', ''),
+            'user_monthly_income': self.validated_data.get('user_monthly_income', ''),
+            'user_monthly_expenses': self.validated_data.get('user_monthly_expenses', ''),
+        } 
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'groups', 'user_permissions']
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'nickname', 'user_point', 'rank',)
