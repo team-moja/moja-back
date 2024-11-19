5d0a1331ac27d09a5f2abba2743d87de6043336a
yunjaeeun <yunjaeeun12@naver.com>
2024-11-19 13:45:39 +0900

    Resolves: 회원 조회 기능 구현
    1. 회원 단일 조회 회원 등급 추가
    2. 회원 다중 조회(아이디, 닉네임, 포인트, 등급추가)
    
    Resolves: #003

--- moja_back/accounts/serializers.py
+++ moja_back/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
-from .models import User
+from .models import User, UserRank
 
 class CustomRegisterSerializer(RegisterSerializer):
     nickname = serializers.CharField(required=True, max_length=255)
@@ -19,12 +19,21 @@ class CustomRegisterSerializer(RegisterSerializer):
             'user_monthly_expenses': self.validated_data.get('user_monthly_expenses', ''),
         } 
 
+class UserRankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRank
+        fields = ('user_rank',)
+
 class UserDetailSerializer(serializers.ModelSerializer):
+    rank = UserRankSerializer()
+
     class Meta:
         model = User
         exclude = ['password', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'groups', 'user_permissions']
 
 class UserListSerializer(serializers.ModelSerializer):
+    rank = UserRankSerializer()
+    
     class Meta:
         model = User
         fields = ('username', 'nickname', 'user_point', 'rank',)
